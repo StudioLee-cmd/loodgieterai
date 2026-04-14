ded07f235bb5335877625e58ba2d5a270eb04df5
StudioLee-cmd <Tim@studiolee.nl>
2026-04-14 21:19:52 +0200

    Cookie banner: proper React component (fixes rendering)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,7 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 
 import "./globals.css";
 import Script from 'next/script';
+import CookieBanner from "@/components/CookieBanner";
 
 const manrope = Manrope({ subsets: ['latin'] });
 const sourceSans = Source_Sans_3({ subsets: ['latin'] });
@@ -64,11 +65,7 @@ export default function RootLayout({
           <Footer />
           <Analytics />
         </ThemeProvider>
-          <Script
-            src="/cookie-banner.js"
-            data-widget-id="69c4ef18d9121c5778d93245"
-            strategy="afterInteractive"
-          />
+          <CookieBanner widgetId="69c4ef18d9121c5778d93245" />
       </body>
     </html>
   );
